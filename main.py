74e7cc8a04f348b3fe698a8bdc0c71eb103de808
Jacob Riske <jacobriske@gmail.com>
2026-04-05 10:47:02 -0700

    add in dt and circlshape

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def main():
     print(f"Screen width: {constants.SCREEN_WIDTH}\nScreen height: {constants.SCREEN_HEIGHT}\n")
 
     pygame.init()
+    timeClock = pygame.time.Clock()
+    dt = 0
 
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
 
@@ -18,6 +20,10 @@ def main():
         screen.fill("black")
 
         pygame.display.flip()
+
+        timeLaps = timeClock.tick(60)
+        dt += timeLaps / 1000
+
 
 if __name__ == "__main__":
     main()
